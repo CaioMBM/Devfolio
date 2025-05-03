0198f4f1f93f42897f7e522deb81cffdddb0d24f
Caio Montenegro <caiombmonteiro@gmail.com>
2025-05-03 15:53:25 -0300

    Adição do modal "Portifólio 1" e de suas funções de abrir e fechar

--- js/projetos.js
+++ js/projetos.js
@@ -2,7 +2,7 @@
 const cardRecriandoSite  = document.querySelectorAll(".containerProjetos div")[0];
 const modalRecriandoSite = document.querySelectorAll(".modalProjeto")[0];
 const modalFechar        = document.querySelectorAll(".icon-x")[0];
-const camadaPreta        = document.querySelectorAll('.camadaPreta')[0];
+const camadaPreta        = document.querySelector('.camadaPreta');
 
 
 // Abrir modal
@@ -20,3 +20,25 @@ function fecharRecriandoSite(){
     modalRecriandoSite.style.visibility = 'hidden';
 }
 modalFechar.addEventListener("click", fecharRecriandoSite);
+
+
+// Modal (Projeto Maior) - Portifólio 1
+const cardPortifolio     = document.querySelectorAll(".containerProjetos div")[1];
+const modalPortifolio    = document.querySelectorAll(".modalProjeto")[1];
+const esconderPortifolio = document.querySelectorAll(".icon-x")[1];
+
+// Abrir modal
+function abrirPortifolio(){
+    camadaPreta.style.visibility     = 'visible';
+    camadaPreta.style.opacity        = 100;
+    modalPortifolio.style.visibility = 'visible';
+}
+cardPortifolio.addEventListener("click", abrirPortifolio);
+
+// Fechar modal
+function fecharPortifolio(){
+    camadaPreta.style.visibility     = 'hidden';
+    camadaPreta.style.opacity        = 0;
+    modalPortifolio.style.visibility = 'hidden';
+}
+esconderPortifolio.addEventListener("click", fecharPortifolio);
