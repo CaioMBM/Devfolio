706cf0833cc42783ed13ae48f121feca4f44e93b
Caio Montenegro <caiombmonteiro@gmail.com>
2025-05-12 16:00:02 -0300

    Adição da main Experiência + Certificados na Sobre (total) pt final
    - Fiz com que cada linha vertical tenha os 25px a mais de height que o height do respectivo card deve ter, antes tinha apenas na linha vertical do primeiro card.
    
    Obs: O último card não tem linha vertical,

--- js/sobre.js
+++ js/sobre.js
@@ -3,6 +3,7 @@ const linhasVertical   = document.querySelectorAll(".linhaVertical");
 const iconCertificado  = document.querySelectorAll(".icon-certificado");
 const cardCertificados = document.querySelectorAll(".cardCertificados");
 
+// Formação - ADS
 // Faz com que a altura da linha seja sempre 25px a mais que a altura do card respectivo
 function ajustarLinhaADS() {
     const alturaADS     = cardCertificados[0].offsetHeight;
@@ -10,8 +11,67 @@ function ajustarLinhaADS() {
     
     linhasVertical[0].style.height = (alturaADS - alturaIconADS + 25 ) + 'px';
 }
-
 // Garante que o JavaScript só vá medir e ajustar a altura depois que toda a página ser carregada
 window.addEventListener('load', ajustarLinhaADS);
 // Recalcula automaticamente sempre que a janela for redimensionada
 window.addEventListener('resize', ajustarLinhaADS);
+
+// Certificado - Conferência
+function ajustarLinhaConferencia() {
+    const alturaConferencia     = cardCertificados[1].offsetHeight;
+    const alturaIconConferencia = iconCertificado[1].offsetHeight;
+    
+    linhasVertical[1].style.height = (alturaConferencia - alturaIconConferencia + 25 ) + 'px';
+}
+window.addEventListener('load', ajustarLinhaConferencia);
+window.addEventListener('resize', ajustarLinhaConferencia);
+
+// Certificado - IA na Gestão
+function ajustarLinhaIAGestao() {
+    const alturaIAGestao     = cardCertificados[2].offsetHeight;
+    const alturaIconIAGestao = iconCertificado[2].offsetHeight;
+    
+    linhasVertical[2].style.height = (alturaIAGestao - alturaIconIAGestao + 25 ) + 'px';
+}
+window.addEventListener('load', ajustarLinhaIAGestao);
+window.addEventListener('resize', ajustarLinhaIAGestao);
+
+// Certificado - Flexbox
+function ajustarLinhaFlexbox() {
+    const alturaFlexbox     = cardCertificados[3].offsetHeight;
+    const alturaIconFlexbox = iconCertificado[3].offsetHeight;
+    
+    linhasVertical[3].style.height = (alturaFlexbox - alturaIconFlexbox + 25 ) + 'px';
+}
+window.addEventListener('load', ajustarLinhaFlexbox);
+window.addEventListener('resize', ajustarLinhaFlexbox);
+
+// Certificado - Excel
+function ajustarLinhaExcel() {
+    const alturaExcel     = cardCertificados[4].offsetHeight;
+    const alturaIconExcel = iconCertificado[4].offsetHeight;
+    
+    linhasVertical[4].style.height = (alturaExcel - alturaIconExcel + 25 ) + 'px';
+}
+window.addEventListener('load', ajustarLinhaExcel);
+window.addEventListener('resize', ajustarLinhaExcel);
+
+// Certificado - UX Unibra
+function ajustarLinhaUX_Unibra() {
+    const alturaUX_Unibra     = cardCertificados[5].offsetHeight;
+    const alturaIconUX_Unibra = iconCertificado[5].offsetHeight;
+    
+    linhasVertical[5].style.height = (alturaUX_Unibra - alturaIconUX_Unibra + 25 ) + 'px';
+}
+window.addEventListener('load', ajustarLinhaUX_Unibra);
+window.addEventListener('resize', ajustarLinhaUX_Unibra);
+
+// Certificado - UX Google
+function ajustarLinhaUX_Google() {
+    const alturaUX_Google     = cardCertificados[6].offsetHeight;
+    const alturaIconUX_Google = iconCertificado[6].offsetHeight;
+    
+    linhasVertical[6].style.height = (alturaUX_Google - alturaIconUX_Google + 25 ) + 'px';
+}
+window.addEventListener('load', ajustarLinhaUX_Google);
+window.addEventListener('resize', ajustarLinhaUX_Google);
