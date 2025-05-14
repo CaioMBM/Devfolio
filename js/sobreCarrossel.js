b4d803f807e7f676cbfeb1edd71c69161d832abd
Caio Montenegro <caiombmonteiro@gmail.com>
2025-05-14 15:24:33 -0300

    Adição da responsividade do carrossel na main Certificados na Sobre
    - Tive que diminuir o tamanho e add um margin-left nos ícones de "Certificados e Formação" porque para telas com width: até 389px e com width:   459px - 598px estavam cortando o ícone quando o carrossel era rolado para direita;
    - Também acrescentei suavidade ao passar os itens, esqueci de ter feito isso antes.

--- js/sobreCarrossel.js
+++ js/sobreCarrossel.js
@@ -1,14 +1,15 @@
-/* CARROSSEL - Experiência */
-const expSetaDireita  = document.querySelectorAll(".carrosselSetas")[1];
-const expSetaEsquerda = document.querySelectorAll(".carrosselSetas")[0];
-const expContainerCarrossel = document.querySelector("#Experiencia section");
-const expScrollMax = expContainerCarrossel.scrollWidth - expContainerCarrossel.clientWidth;
-
 // Media query
 const tela_Max390px       = window.matchMedia("(max-width: 390px)");
 const tela_Min390Max458px = window.matchMedia("(min-width: 390px) and (max-width: 458px)");
 const tela_Min459Max598px = window.matchMedia("(min-width: 459px) and (max-width: 598px)");
 const tela_Min599Max768px = window.matchMedia("(min-width: 599px) and (max-width: 768px)");
+
+
+/* CARROSSEL - Experiência */
+const expSetaDireita  = document.querySelectorAll(".carrosselSetas")[1];
+const expSetaEsquerda = document.querySelectorAll(".carrosselSetas")[0];
+const expContainerCarrossel = document.querySelector("#Experiencia section");
+const expScrollMax = expContainerCarrossel.scrollWidth - expContainerCarrossel.clientWidth;
 
 // Mover para direita
 function expCarrosselMoverDireita(){ 
@@ -124,30 +125,95 @@ const certScrollMax = certContainerCarrossel.scrollWidth - certContainerCarrosse
 
 // Mover para direita
 function certCarrosselMoverDireita(){ 
-    console.log("Mover direira")
-    
-    // Se estiver no último card e então clicar para mover para direita, será mostrado o 1º card do carrossel
-    if (certContainerCarrossel.scrollLeft >= certScrollMax) {
-        certContainerCarrossel.scrollLeft = 0;
+    // Tela ... - 390px
+    if(tela_Max390px.matches){
+        if (certContainerCarrossel.scrollLeft >= certScrollMax) {
+            certContainerCarrossel.scrollLeft = 0;
+        } 
+        else {
+            certContainerCarrossel.scrollLeft += 330;
+        }
     }
-    // Se não for o último card, será mostrado o card seguinte
-    else {
-        certContainerCarrossel.scrollLeft += 460; // Esse nº vem do width do card + gap entre os cards, então ele avançará um card
+    // Tela 390px - 458px
+    else if(tela_Min390Max458px.matches){
+        if (certContainerCarrossel.scrollLeft >= certScrollMax) {
+            certContainerCarrossel.scrollLeft = 0;
+        } 
+        else {
+            certContainerCarrossel.scrollLeft += 360;
+        }
     }
-    
+    // Tela 459px - 598px
+    else if(tela_Min459Max598px.matches){
+        if (certContainerCarrossel.scrollLeft >= certScrollMax) {
+            certContainerCarrossel.scrollLeft = 0;
+        } 
+        else {
+            certContainerCarrossel.scrollLeft += 430;
+        }
+    }
+    // Tela 599px - 768px
+    else if(tela_Min599Max768px.matches){
+        if (certContainerCarrossel.scrollLeft >= certScrollMax) {
+            certContainerCarrossel.scrollLeft = 0;
+        } 
+        else {
+            certContainerCarrossel.scrollLeft += 460; 
+        }
+    }
+    // Valor padrão
+    else{
+        // Se estiver no último card e então clicar para mover para direita, será mostrado o 1º card do carrossel
+        if (certContainerCarrossel.scrollLeft >= certScrollMax) {
+            certContainerCarrossel.scrollLeft = 0;
+        }
+        // Se não for o último card, será mostrado o card seguinte
+        else {
+            certContainerCarrossel.scrollLeft += 460; // Esse nº vem do width do card + gap entre os cards, então ele avançará um card
+        }
+    }
 }
 certSetaDireita.addEventListener('click', certCarrosselMoverDireita);
 
 // Mover para esquerda
-function certCarrosselMoverEsquerda(){     
-    // Se estiver no 1º card e então clicar para mover para esquerda, será mostrado o último card do carrossel
-    if (certContainerCarrossel.scrollLeft <= 0) {
-        certContainerCarrossel.scrollLeft = certContainerCarrossel.scrollWidth - certContainerCarrossel.clientWidth;
-    } 
-    // Se não for o 1º card, será mostrado o card anterior
-    else {
-        certContainerCarrossel.scrollLeft -= 460; // Esse nº vem do width do card + gap entre os cards, então ele avançará um card
+function certCarrosselMoverEsquerda(){
+    // Tela ... - 390px
+    if(tela_Max390px.matches){
+        if (certContainerCarrossel.scrollLeft <= 0) {
+            certContainerCarrossel.scrollLeft = certContainerCarrossel.scrollWidth - certContainerCarrossel.clientWidth;
+        } 
+        else {
+            certContainerCarrossel.scrollLeft -= 330;
+        }
     }
-    
+    // Tela 390px - 458px
+    else if(tela_Min390Max458px.matches){
+        if (certContainerCarrossel.scrollLeft <= 0) {
+            certContainerCarrossel.scrollLeft = certContainerCarrossel.scrollWidth - certContainerCarrossel.clientWidth;
+        } 
+        else {
+            certContainerCarrossel.scrollLeft -= 360;
+        }
+    }
+    // Tela 459px - 598px
+    else if(tela_Min459Max598px.matches){
+        if (certContainerCarrossel.scrollLeft <= 0) {
+            certContainerCarrossel.scrollLeft = certContainerCarrossel.scrollWidth - certContainerCarrossel.clientWidth;
+        } 
+        else {
+            certContainerCarrossel.scrollLeft -= 430;
+        }
+    }
+    // Valor padrão
+    else{
+        // Se estiver no 1º card e então clicar para mover para esquerda, será mostrado o último card do carrossel
+        if (certContainerCarrossel.scrollLeft <= 0) {
+            certContainerCarrossel.scrollLeft = certContainerCarrossel.scrollWidth - certContainerCarrossel.clientWidth;
+        } 
+        // Se não for o 1º card, será mostrado o card anterior
+        else {
+            certContainerCarrossel.scrollLeft -= 460; // Esse nº vem do width do card + gap entre os cards, então ele avançará um card
+        }
+    }
 }
 certSetaEsquerda.addEventListener('click', certCarrosselMoverEsquerda);
