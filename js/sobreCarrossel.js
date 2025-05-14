3bda2ba4e7890b95bfaf0ede76813d6fdd55e36d
Caio Montenegro <caiombmonteiro@gmail.com>
2025-05-14 14:46:17 -0300

    Adição da responsividade do carrossel na main Experiência na Sobre

--- js/sobreCarrossel.js
+++ js/sobreCarrossel.js
@@ -4,28 +4,113 @@ const expSetaEsquerda = document.querySelectorAll(".carrosselSetas")[0];
 const expContainerCarrossel = document.querySelector("#Experiencia section");
 const expScrollMax = expContainerCarrossel.scrollWidth - expContainerCarrossel.clientWidth;
 
+// Media query
+const tela_Max390px       = window.matchMedia("(max-width: 390px)");
+const tela_Min390Max458px = window.matchMedia("(min-width: 390px) and (max-width: 458px)");
+const tela_Min459Max598px = window.matchMedia("(min-width: 459px) and (max-width: 598px)");
+const tela_Min599Max768px = window.matchMedia("(min-width: 599px) and (max-width: 768px)");
+
 // Mover para direita
 function expCarrosselMoverDireita(){ 
-    // Se estiver no último card e então clicar para mover para direita, será mostrado o 1º card do carrossel
-    if (expContainerCarrossel.scrollLeft >= expScrollMax) {
-        expContainerCarrossel.scrollLeft = 0;
-    } 
-    // Se não for o último card, será mostrado o card seguinte
-    else {
-        expContainerCarrossel.scrollLeft += 620; // Esse nº vem do width do card + gap entre os cards, então ele avançará um card
+    // Tela ... - 390px
+    if(tela_Max390px.matches){
+        if (expContainerCarrossel.scrollLeft >= expScrollMax) {
+            expContainerCarrossel.scrollLeft = 0;
+        } 
+        else {
+            expContainerCarrossel.scrollLeft += 340;
+        }
+    }
+    // Tela 390px - 458px
+    else if(tela_Min390Max458px.matches){
+        if (expContainerCarrossel.scrollLeft >= expScrollMax) {
+            expContainerCarrossel.scrollLeft = 0;
+        } 
+        else {
+            expContainerCarrossel.scrollLeft += 380;
+        }
+    }
+    // Tela 459px - 598px
+    else if(tela_Min459Max598px.matches){
+        if (expContainerCarrossel.scrollLeft >= expScrollMax) {
+            expContainerCarrossel.scrollLeft = 0;
+        } 
+        else {
+            expContainerCarrossel.scrollLeft += 420;
+        }
+    }
+    // Tela 599px - 768px
+    else if(tela_Min599Max768px.matches){
+        if (expContainerCarrossel.scrollLeft >= expScrollMax) {
+            expContainerCarrossel.scrollLeft = 0;
+        } 
+        else {
+            expContainerCarrossel.scrollLeft += 510;
+        }
+    }
+    // Valor padrão
+    else{
+        // Se estiver no último card e então clicar para mover para direita, será mostrado o 1º card do carrossel
+        if (expContainerCarrossel.scrollLeft >= expScrollMax) {
+            expContainerCarrossel.scrollLeft = 0;
+        } 
+        // Se não for o último card, será mostrado o card seguinte
+        else {
+            expContainerCarrossel.scrollLeft += 620; // Esse nº vem do width do card + gap entre os cards, então ele avançará um card
+        }
     }
 }
 expSetaDireita.addEventListener('click', expCarrosselMoverDireita);
 
 // Mover para esquerda
-function expCarrosselMoverEsquerda(){ 
-    // Se estiver no 1º card e então clicar para mover para esquerda, será mostrado o último card do carrossel
-    if (expContainerCarrossel.scrollLeft <= 0) {
-        expContainerCarrossel.scrollLeft = expContainerCarrossel.scrollWidth - expContainerCarrossel.clientWidth;
+function expCarrosselMoverEsquerda(){
+    // Tela ... - 390px
+    if(tela_Max390px.matches){
+        if (expContainerCarrossel.scrollLeft <= 0) {
+            expContainerCarrossel.scrollLeft = expContainerCarrossel.scrollWidth - expContainerCarrossel.clientWidth;
+        } 
+        else {
+            expContainerCarrossel.scrollLeft -= 340;
+        }
+    }
+    // Tela 390px - 458px
+    else if(tela_Min390Max458px.matches){
+        if (expContainerCarrossel.scrollLeft <= 0) {
+            expContainerCarrossel.scrollLeft = expContainerCarrossel.scrollWidth - expContainerCarrossel.clientWidth;
+        } 
+        else {
+            expContainerCarrossel.scrollLeft -= 380;
+        }
+    }
+    // Tela 459px - 598px
+    else if(tela_Min459Max598px.matches){
+        if (expContainerCarrossel.scrollLeft <= 0) {
+            expContainerCarrossel.scrollLeft = expContainerCarrossel.scrollWidth - expContainerCarrossel.clientWidth;
+        } 
+        else {
+            expContainerCarrossel.scrollLeft -= 420;
+        }
+    }
+    // Tela 599px - 768px
+    else if(tela_Min599Max768px.matches){
+        if (expContainerCarrossel.scrollLeft <= 0) {
+            expContainerCarrossel.scrollLeft = expContainerCarrossel.scrollWidth - expContainerCarrossel.clientWidth;
+        } 
+        else {
+            expContainerCarrossel.scrollLeft -= 510;
+        }
     } 
-    // Se não for o 1º card, será mostrado o card anterior
+    // Valor padrão
     else {
-        expContainerCarrossel.scrollLeft -= 620; // Esse nº vem do width do card + gap entre os cards, então ele avançará um card
+
+        // Se estiver no 1º card e então clicar para mover para esquerda, será mostrado o último card do carrossel
+        if (expContainerCarrossel.scrollLeft <= 0) {
+            expContainerCarrossel.scrollLeft = expContainerCarrossel.scrollWidth - expContainerCarrossel.clientWidth;
+        } 
+        // Se não for o 1º card, será mostrado o card anterior
+        else {
+            expContainerCarrossel.scrollLeft -= 620; // Esse nº vem do width do card + gap entre os cards, então ele avançará um card
+        }
     }
 }
 expSetaEsquerda.addEventListener('click', expCarrosselMoverEsquerda);
