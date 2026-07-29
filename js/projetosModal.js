bff7fe320fd59a5517320747f177be4918d6d16b
Caio Montenegro <caiombmonteiro@gmail.com>
2026-07-29 15:35:56 -0300

    refactor(projetos-modal): Diminuição do tamanho do código JS dos modais
    - De 135 linhas para 29 linhas.

--- js/projetosModal.js
+++ js/projetosModal.js
@@ -3,133 +3,27 @@ const camadaPretaPMa = document.querySelector('.camadaPreta');
 
 
 /* MODAL */
-// Modal - Recriando Site
-const cardRecriandoSite  = document.querySelectorAll(".containerProjetos div")[0];
-const modalRecriandoSite = document.querySelectorAll(".modalProjeto")[0];
-const modalFechar        = document.querySelectorAll(".icon-x")[0];
+const cardProjeto  = document.querySelectorAll(".containerProjetos div");  // nodelist com todos os cards
+const modalProjeto = document.querySelectorAll(".modalProjeto");           // nodelist com todos os modais
+const modalIconFechar    = document.querySelectorAll(".icon-x");           // nodelist com todos os ícones de fechar modal
 
-// Abrir modal
-function abrirRecriandoSite(){
-    camadaPretaPMa.style.visibility = 'visible';
-    camadaPretaPMa.style.opacity    = 100;
-    modalRecriandoSite.style.visibility   = 'visible';
+
+// Loop para a pegar o índice do elemento clicado e sofrer ação no elemento respectivo, ou seja, se clicar no primeiro card, ent será aberto o primeiro modal.
+for (let c = 0; c < cardProjeto.length; c++){
+    // Abrir modal
+    function abrirModal(){
+        camadaPretaPMa.style.visibility = 'visible';
+        camadaPretaPMa.style.opacity    = 100;
+        modalProjeto[c].style.visibility = 'visible';
+    }
+
+    // Fechar modal
+    function fecharModal(){
+        camadaPretaPMa.style.visibility = '';
+        camadaPretaPMa.style.opacity    = 0;
+        modalProjeto[c].style.visibility = 'hidden';
+    }
+
+    cardProjeto[c].addEventListener('click', abrirModal);
+    modalIconFechar[c].addEventListener('click', fecharModal);
 }
-cardRecriandoSite.addEventListener("click", abrirRecriandoSite);
-
-// Fechar modal
-function fecharRecriandoSite(){
-    camadaPretaPMa.style.visibility = 'hidden';
-    camadaPretaPMa.style.opacity    = 0;
-    modalRecriandoSite.style.visibility  = 'hidden';
-}
-modalFechar.addEventListener("click", fecharRecriandoSite);
-
-
-// Modal - FlexBLog
-const cardFlexBLog     = document.querySelectorAll(".containerProjetos div")[1];
-const modalFlexBLog    = document.querySelectorAll(".modalProjeto")[1];
-const esconderFlexBLog = document.querySelectorAll(".icon-x")[1];
-
-// Abrir modal
-function abrirFlexBLog(){
-    camadaPretaPMa.style.visibility = 'visible';
-    camadaPretaPMa.style.opacity    = 100;
-    modalFlexBLog.style.visibility        = 'visible';
-}
-cardFlexBLog.addEventListener("click", abrirFlexBLog);
-
-// Fechar modal
-function fecharFlexBLog(){
-    camadaPretaPMa.style.visibility = 'hidden';
-    camadaPretaPMa.style.opacity    = 0;
-    modalFlexBLog.style.visibility        = 'hidden';
-}
-esconderFlexBLog.addEventListener("click", fecharFlexBLog);
-
-
-// Modal - Eduadro Brito (Freelance)
-const cardEduadroBrito     = document.querySelectorAll(".containerProjetos div")[2];
-const modalEduadroBrito    = document.querySelectorAll(".modalProjeto")[2];
-const esconderEduadroBrito = document.querySelectorAll(".icon-x")[2];
-
-// Abrir modal
-function abrirEduadroBrito(){
-    camadaPretaPMa.style.visibility = 'visible';
-    camadaPretaPMa.style.opacity    = 100;
-    modalEduadroBrito.style.visibility    = 'visible';
-}
-cardEduadroBrito.addEventListener("click", abrirEduadroBrito);
-
-// Fechar modal
-function fecharEduadroBrito(){
-    camadaPretaPMa.style.visibility = 'hidden';
-    camadaPretaPMa.style.opacity    = 0;
-    modalEduadroBrito.style.visibility    = 'hidden';
-}
-esconderEduadroBrito.addEventListener("click", fecharEduadroBrito);
-
-
-// Modal - Prevent Elevadores (Freelance)
-const cardPreventElevadores     = document.querySelectorAll(".containerProjetos div")[3];
-const modalPreventElevadores    = document.querySelectorAll(".modalProjeto")[3];
-const esconderPreventElevadores = document.querySelectorAll(".icon-x")[3];
-
-// Abrir modal
-function abrirPreventElevadores(){
-    camadaPretaPMa.style.visibility   = 'visible';
-    camadaPretaPMa.style.opacity      = 100;
-    modalPreventElevadores.style.visibility = 'visible';
-}
-cardPreventElevadores.addEventListener("click", abrirPreventElevadores);
-
-// Fechar modal
-function fecharPreventElevadores(){
-    camadaPretaPMa.style.visibility   = 'hidden';
-    camadaPretaPMa.style.opacity      = 0;
-    modalPreventElevadores.style.visibility = 'hidden';
-}
-esconderPreventElevadores.addEventListener("click", fecharPreventElevadores);
-
-
-// Modal - RPI (Freelance)
-const cardRPI     = document.querySelectorAll(".containerProjetos div")[4];
-const modalRPI    = document.querySelectorAll(".modalProjeto")[4];
-const esconderRPI = document.querySelectorAll(".icon-x")[4];
-
-// Abrir modal
-function abrirRPI(){
-    camadaPretaPMa.style.visibility   = 'visible';
-    camadaPretaPMa.style.opacity      = 100;
-    modalRPI.style.visibility = 'visible';
-}
-cardRPI.addEventListener("click", abrirRPI);
-
-// Fechar modal
-function fecharRPI(){
-    camadaPretaPMa.style.visibility   = 'hidden';
-    camadaPretaPMa.style.opacity      = 0;
-    modalRPI.style.visibility = 'hidden';
-}
-esconderRPI.addEventListener("click", fecharRPI);
-
-
-// Modal - ByCar Auto Premium (Freelance)
-const cardByCar     = document.querySelectorAll(".containerProjetos div")[5];
-const modalByCar    = document.querySelectorAll(".modalProjeto")[5];
-const esconderByCar = document.querySelectorAll(".icon-x")[5];
-
-// Abrir modal
-function abrirByCar(){
-    camadaPretaPMa.style.visibility   = 'visible';
-    camadaPretaPMa.style.opacity      = 100;
-    modalByCar.style.visibility = 'visible';
-}
-cardByCar.addEventListener("click", abrirByCar);
-
-// Fechar modal
-function fecharByCar(){
-    camadaPretaPMa.style.visibility   = 'hidden';
-    camadaPretaPMa.style.opacity      = 0;
-    modalByCar.style.visibility = 'hidden';
-}
-esconderByCar.addEventListener("click", fecharByCar);
